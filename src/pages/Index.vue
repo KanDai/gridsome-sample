44c099f8fc644663aab20b870f0ba616d83af2c0
Daichi Kanke <kanke@tam-tam.co.jp>
2020-01-03 21:28:11 +0900

    Vue構文試し

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,6 +6,8 @@
 
     <h1>Hello, world!</h1>
 
+    <h2>{{ message }}</h2>
+
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>
@@ -22,6 +24,12 @@
 export default {
   metaInfo: {
     title: 'Hello, world!'
+  },
+
+  data() {
+    return {
+      message : 'こんにちは',
+    }
   }
 }
 </script>
